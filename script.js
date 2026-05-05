84cd6c79a578f6e066b59ff88232bc40bc3daf46
benny33-cmd <broccolinainformatica@gmail.com>
2026-05-05 20:32:40 +0200

    Update database URL in DatabasePrenotazioni class

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class DatabasePrenotazioni {
     constructor() {
-        this.url = "AKfycbzqiFbj0DxSYVySVp9qpzH6K_400H82y5p7Hb-PvACCR8K3KSOGNF2ivbh7wEVxSjQf";
+        this.url = "https://script.google.com/macros/library/d/1DyVqZ3o8wVP6uH20FxRYCYAsX9ua59gWTzcgNSnydrP4sHqe8cuCzMIp/4";
         this.prenotazioni = [];
         this.lista = document.getElementById("lista");
 
