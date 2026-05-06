3df811ef493204ae9f4b33abb9afd79ad5b7c3f9
benny33-cmd <broccolinainformatica@gmail.com>
2026-05-06 15:25:39 +0200

    Remove 'html=true' from URL in DatabasePrenotazioni

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function cambiaSlide(btn, direzione) {
 ========================= */
 class DatabasePrenotazioni {
     constructor() {
-        this.url = "https://script.google.com/macros/s/AKfycbwxLq6impTO7SOiV-Qt_rhexyUh3UPS0OH4ZnH6Z1yFge50uZ9-hpoPsKJULdAnpGc/exec?html=true";
+        this.url = "https://script.google.com/macros/s/AKfycbwxLq6impTO7SOiV-Qt_rhexyUh3UPS0OH4ZnH6Z1yFge50uZ9-hpoPsKJULdAnpGc/exec";
         this.prenotazioni = [];
 
         this.carica();
